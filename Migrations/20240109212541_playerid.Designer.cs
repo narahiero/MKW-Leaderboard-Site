12ebd3114dcbea8cc0b7a04217725f430e5c2b30
Arvo Koskikallio <arvo.koskikallio@primapower.onmicrosoft.com>
2024-01-09 23:38:44 +0200

    playerid

--- Migrations/20240109212541_playerid.Designer.cs
+++ Migrations/20240109212541_playerid.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using my_app.Services.Database;
 
@@ -11,9 +12,10 @@ using my_app.Services.Database;
 namespace my_app.Migrations
 {
     [DbContext(typeof(MKWLeaderboardContext))]
-    partial class MKWLeaderboardContextModelSnapshot : ModelSnapshot
+    [Migration("20240109212541_playerid")]
+    partial class playerid
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- Models/Time.cs
+++ Models/Time.cs
@@ -10,8 +10,9 @@ namespace my_app.Models
 
         }
 
-        public Time(DateTime date, Track track, bool glitch, bool flap, int minutes, int seconds, int milliseconds, string link, string ghost) {
+        public Time(DateTime date, int playerId, Track track, bool glitch, bool flap, int minutes, int seconds, int milliseconds, string link, string ghost) {
             Date = date;
+            PlayerId = playerId;
             Track = track;
             Glitch = glitch;
             Flap = flap;
@@ -25,6 +26,9 @@ namespace my_app.Models
         public int Id { get; set; }
 
         public DateTime? Date { get; set; }
+
+        [Required]
+        public int PlayerId { get; set; }
 
         [Required]
         public Track Track { get; set; }
