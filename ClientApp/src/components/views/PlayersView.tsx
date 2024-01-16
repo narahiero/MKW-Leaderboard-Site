5a2d64f6d5211f15ab7f1551c0596e9af544db2b
Arvo Koskikallio <arvo.koskikallio@primapower.onmicrosoft.com>
2024-01-16 14:00:29 +0200

    fix player fetch loop

--- ClientApp/src/components/views/PlayersView.tsx
+++ ClientApp/src/components/views/PlayersView.tsx
@@ -6,24 +6,25 @@ import Navbar from '../common/Navbar';
 import PlayerTable from '../tables/PlayerTable';
 
 const PlayersView = () => {
-    const [players, setPlayers] = useState<Player[]>([]);
+  const [players, setPlayers] = useState<Player[]>([]);
 
-    useEffect(() => {
+  useEffect(() => {
     const fetchData = async () => {
-        const players = await getPlayers();
-        setPlayers(players);
+      const fetchedPlayers = await getPlayers();
+      setPlayers(fetchedPlayers);
     };
-    fetchData();
-    });
 
-    return (
-        <div>
-        <Navbar />
-            <div className="table-container">
-            <PlayerTable players={players}/>
-            </div>
-        </div>
-    );
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <Navbar />
+      <div className="table-container">
+        <PlayerTable players={players} />
+      </div>
+    </div>
+  );
 };
 
 export default PlayersView;
